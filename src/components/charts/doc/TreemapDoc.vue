e7bf260f1b4506eb9f207ba40c20b68918405d30
Thomas Froment <thomasfroment2@outlook.fr>
2025-05-19 15:08:38 +0200

    chore: Improve wording in TreemapDoc.vue

--- src/components/charts/doc/TreemapDoc.vue
+++ src/components/charts/doc/TreemapDoc.vue
@@ -7,7 +7,7 @@ defineProps<{
 <template>
     <p v-html="`Ce diagramme représente les résultats de la campagne du <b>CSV n°${id + 1}.</b>`" />
     <p>
-        Chaque zone du treemap correspond à un service de l’organisation, puis est subdivisée selon les résultats de la
-        campagne au sein du service.
+        Chaque zone de l'arborescence correspond à un service de l’organisation, puis est subdivisée selon les résultats
+        de la campagne au sein des services.
     </p>
 </template>
